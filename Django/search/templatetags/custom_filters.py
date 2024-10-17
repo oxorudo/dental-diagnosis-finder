6780c23c682d0c80085f5023ade4d8b24e2588a5
Seungyeon <seongyon98@gmail.com>
2024-10-17 22:37:32 +0900

    코드 대신 병명으로 검색창 전달로 수정 ...templatetags는 아직 의도한 대로 작동 못함

--- Django/search/templatetags/custom_filters.py
+++ Django/search/templatetags/custom_filters.py
@@ -1,8 +1,20 @@
+import re
 from django import template
 
 register = template.Library()
 
 @register.filter
-def split(value, delimiter):
-    """Returns the value split by the given delimiter."""
-    return value.split(delimiter)
+def get_subcategories(middle_data):
+    """하위 분류만 반환하는 필터."""
+    pattern = r'^[A-Z][0-9]{2}\.[0-9]{2}'
+    subcategories = {}
+
+    # middle_data가 dict인 경우 처리
+    if isinstance(middle_data, dict) and 'children' in middle_data:
+        children = middle_data['children']
+        
+        for code, data in children.items():
+            if re.search(pattern, code):
+                subcategories[code] = data
+
+    return subcategories
