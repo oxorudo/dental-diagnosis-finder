59fd7f00e3d17e26fc36546c6ddfdee6b1a0af8a
online5880 <gjtjqkr1@naver.com>
2024-10-18 14:31:37 +0900

    api key 경로 받아오는 함수 추가
    - Djang 폴더 내에 key.json 을 찾는다.

--- Django/search/apps.py
+++ Django/search/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
-from .utils.GoogleSheet import get_sheet_data,get_url
+from .utils.GoogleSheet import get_sheet_data,get_url,find_key_path
 from .utils.QueryCorrection import HangulSearch
 
 class SearchConfig(AppConfig):
@@ -14,6 +14,7 @@ class SearchConfig(AppConfig):
         global global_searcher
         global global_url
         # 서버 시작 시 데이터 불러오기
-        global_sheet_data = get_sheet_data("../NLP/key.json")
+        api_key_path = find_key_path()
+        global_sheet_data = get_sheet_data(api_key_path)
         global_searcher = HangulSearch(global_sheet_data)
-        global_url = get_url()
+        global_url = get_url(api_key_path)
